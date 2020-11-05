a9ca7d603b8c3075280db2f2288bc9fa8fe5e0f4
Sebastian Falborg <sebastianfalborg@gmail.com>
2020-11-05 16:57:48 +0100

    First commit

---
+++ DuravaliaWiki.App/Models/Place.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuravaliaWiki.App.Models
+{
+    public class Place
+    {
+		public string Name { get; set; }
+
+		public string Description { get; set; }
+
+		public int XStartCoordinate { get; set; }
+
+		public int YStartCoordinate { get; set; }
+
+		public int XEndCoordinate { get; set; }
+
+		public int YEndCoordinate { get; set; }
+
+	}
+}
